d7db707d55bce8482c8ec28afea22f866c35a00f
jgerardsimcock <justin_simcock@yahoo.com>
2015-12-28 15:03:33 -0800

    add invalid email test

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  validates(:name, presence: true, length: {maximum: 50}), 
+  validates(:email, presence: true, length: {maximum: 255})
+  validates(:role, presence: true, length: {maximum: 50})
+  
+end
