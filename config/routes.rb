f4a89ff6b62327824f377018b348d26ce9bd0a1f
jgerardsimcock <justin_simcock@yahoo.com>
2016-01-05 19:20:48 -0800

    add sessions helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   
-  root to: 'static_pages#root'
-  get '/about', to: 'static_pages#about'
-  get '/events', to: 'static_pages#events'
-  get '/signup', to: 'users#new'
+  
+  root                'static_pages#home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 end
