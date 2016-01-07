7ab89a27a86ea9720ce14ce1633da50dd5f1ceff
jgerardsimcock <justin_simcock@yahoo.com>
2016-01-07 09:21:22 -0800

    password reset renders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
   
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root                'static_pages#root'
   get    'about'   => 'static_pages#about'
   get    'events' => 'static_pages#events'
@@ -10,4 +14,5 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
