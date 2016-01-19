60ea575f7becd3755558d9c6c2e59a35bce41cac
Akansh Murthy <ak45m16@gmail.com>
2016-01-18 20:10:00 -0800

    add association and test

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @event = Event.new(title: "Health 2.0", location: "San Fran",
+                       url: "www.health2sf.com", time: "2016-01-01 20:00:00")
+  end
+
+  test "should be valid" do
+    assert @event.valid?
+  end
+
+  test "title should be present" do
+    @event.title = nil
+    assert_not @event.valid?
+  end
+
 end
