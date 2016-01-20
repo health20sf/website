c0b74c22d86559da8e1b666aa3088abaaa655661
jgerardsimcock <justin_simcock@yahoo.com>
2016-01-20 08:35:19 -0800

    r

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20160118163106) do
 
-ActiveRecord::Schema.define(version: 20160107164849) do
+  create_table "events", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.string   "location",   null: false
+    t.string   "url"
+    t.datetime "time",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -30,18 +38,5 @@ ActiveRecord::Schema.define(version: 20160107164849) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-
-
-ActiveRecord::Schema.define(version: 20160118163106) do
-
-  create_table "events", force: :cascade do |t|
-    t.string   "title",      null: false
-    t.string   "location",   null: false
-    t.string   "url"
-    t.datetime "time",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 
 end
