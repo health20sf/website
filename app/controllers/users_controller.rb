9243669a314b377c40d2bdd12f15720202367e53
Akansh Murthy <ak45m16@gmail.com>
2018-05-22 21:21:39 -0700

    Update UI and UX a bit more

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.all
+    @users = User.all.includes(:profile)
   end
 
   def show
