ea834bffd5aa232dd24bc453088ec776de714648
Akansh Murthy <ak45m16@gmail.com>
2018-05-22 21:39:24 -0700

    Remove unneeded styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.all.includes(:profile)
+    @users = User.all
   end
 
   def show
